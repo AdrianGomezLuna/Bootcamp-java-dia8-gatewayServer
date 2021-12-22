a1b1d3c8883483d042cfb03eb3c67787cdd503aa
AdrianGomezLuna <adrigoluna@gmail.com>
2021-12-22 13:42:19 +0100

    first commit

---
+++ src/main/java/com/nttdata/boocamp/gateway/gatewayserver/filters/Filter1.java
@@ -0,0 +1,31 @@
+package com.nttdata.boocamp.gateway.gatewayserver.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class Filter1 implements GlobalFilter, Ordered{
+
+    Logger logger = LoggerFactory.getLogger(Filter1.class);
+	
+	@Override
+	public int getOrder() {
+		return 1;
+	}
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		logger.info("primer pre flitro");
+		return chain.filter(exchange).then(Mono.fromRunnable(() ->        
+			logger.info("first post filter")
+        ));
+		
+	}
+}
